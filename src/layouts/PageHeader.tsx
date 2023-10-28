12d874c9fda095f400e4c27755f45ad239265835
zguo0040 <zguo0040@student.monash.edu>
2023-10-29 01:08:53 +1100

    main function done

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -2,30 +2,18 @@ import React, { useState } from "react";
 import logo from "../assets/Logo.png";
 import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
 import { Button } from "../components/Button";
+import { useSidebarContext } from "../contexts/SidebarContext";
 
 export default function PageHeader() {
   const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div
-        className={` gap-4 items-center flex-shrink-0  ${
-          showFullWidthSearch ? "hidden" : "flex"
-        }`}
-      >
-        <Button variant="ghost" size="icon">
-          <Menu />
-        </Button>
-
-        <a href="/">
-          <img src={logo} className="h-6" alt="" />
-        </a>
-      </div>
+      <PageHeaderFirstSection hidden={showFullWidthSearch} />{" "}
       <form
         className={`md:flex  gap-4 flex-grow justify-center ${
           showFullWidthSearch ? "flex" : "hidden md:flex"
         }`}
       >
-        {" "}
         <Button
           size="icon"
           variant="ghost"
@@ -76,6 +64,30 @@ export default function PageHeader() {
           <User />
         </Button>
       </div>
+    </div>
+  );
+}
+type PageHeaderFirstSectionProps = {
+  hidden?: boolean;
+};
+export function PageHeaderFirstSection({
+  hidden = false,
+}: PageHeaderFirstSectionProps) {
+  const { toggle } = useSidebarContext();
+
+  return (
+    <div
+      className={` gap-4 items-center flex-shrink-0  ${
+        hidden ? "hidden" : "flex"
+      }`}
+    >
+      <Button onClick={toggle} variant="ghost" size="icon">
+        <Menu />
+      </Button>
+
+      <a href="/">
+        <img src={logo} className="h-6" alt="" />
+      </a>
     </div>
   );
 }
